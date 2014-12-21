aa9306b11ed3a0c135f487a9929ddbdfc5702b3c
Kyle Noland <kylenoland@gmail.com>
2014-12-20 21:38:25 -0600

    Initial commit

---
+++ src/KyleNoland/LaravelHelper/LaravelHelperServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace KyleNoland\LaravelHelper;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelHelperServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
